d567444cd255c4a89fe483b5b236fa7b0e069999
dominichsiao <72414659+dominichsiao@users.noreply.github.com>
2021-05-27 17:21:40 -0700

    3rd commit that tests all the conditions

--- test_leap_year.py
+++ test_leap_year.py
@@ -3,7 +3,10 @@ import leap_year
 
 class classTestLeapYear(unittest.TestCase):
     def test_leap_year_1(self):
-        self.assertEqual(leap_year.check_year(4000), "40 is a leap year")
+        self.assertEqual(leap_year.check_year(4000), "4000 is a leap year")
 
     def test_leap_year_2(self):
         self.assertEqual(leap_year.check_year(4500), "4500 is not a leap year")
+
+    def test_leap_year_3(self):
+        self.assertEqual(leap_year.check_year(2000), "2000 is a leap year")
