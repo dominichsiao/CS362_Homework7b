828709383314287361ac83689c244d1989d436eb
dominichsiao <72414659+dominichsiao@users.noreply.github.com>
2021-05-27 17:22:22 -0700

    3rd commit that checks mod 400

--- leap_year.py
+++ leap_year.py
@@ -9,12 +9,18 @@ def check_year(a):
     #print(a)
     if (a % 4 == 0):
         if (a % 100 == 0):
-            b = 0
+            if (a % 400 == 0):
+                b = 1
+
+            else:
+                b = 0
+
         else:
             b = 1
 
     else:
         b = 0
+
 
     if (b == 1):
         return(str(a) + " is a leap year")
