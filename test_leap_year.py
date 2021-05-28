398016421257f99b464995cfa541662ce33df3e4
dominichsiao <72414659+dominichsiao@users.noreply.github.com>
2021-05-27 17:14:09 -0700

    2nd commit that tests mod 100

--- test_leap_year.py
+++ test_leap_year.py
@@ -3,4 +3,7 @@ import leap_year
 
 class classTestLeapYear(unittest.TestCase):
     def test_leap_year_1(self):
-        self.assertEqual(leap_year.check_year(40), "40 is a leap year")
+        self.assertEqual(leap_year.check_year(4000), "40 is a leap year")
+
+    def test_leap_year_2(self):
+        self.assertEqual(leap_year.check_year(4500), "4500 is not a leap year")
